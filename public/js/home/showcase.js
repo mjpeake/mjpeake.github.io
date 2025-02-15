507fed1a08d5b2bf77da238db184d3822da5a208
mjpeake <peake.matthew@gmail.com>
2025-02-15 23:44:19 +0000

    Updatign flowfields package and gallery images

---
+++ public/js/home/showcase.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    const color = $('#home-showcase').css("color");
+    const bgColor = $('#home-showcase').css("background-color");
+    ColourfulBoids('home-showcase', color, bgColor);
+});
