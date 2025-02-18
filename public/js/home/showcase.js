4b6ef6dcb4e8d839272414665c11981481037d5b
mjpeake <peake.matthew@gmail.com>
2025-02-18 22:42:12 +0000

    Uncommitting public directory

--- public/js/home/showcase.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    const color = $('#home-showcase').css("color");
-    const bgColor = $('#home-showcase').css("background-color");
-    ColourfulBoids('home-showcase', color, bgColor);
-});
